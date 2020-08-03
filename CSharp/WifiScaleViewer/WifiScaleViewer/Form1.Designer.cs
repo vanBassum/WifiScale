ddee1b30e809b19a0ad972801a8a3d8f10d17dae
Bas Visscher <bas.visscher24@gmail.com>
2020-08-03 22:06:09 +0200

    Added project to view log of scale

--- CSharp/WifiScaleViewer/WifiScaleViewer/Form1.Designer.cs
+++ CSharp/WifiScaleViewer/WifiScaleViewer/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            FRMLib.Scope.Controls.ScopeViewSettings scopeViewSettings2 = new FRMLib.Scope.Controls.ScopeViewSettings();
+            this.scopeView1 = new FRMLib.Scope.Controls.ScopeView();
+            this.traceView1 = new FRMLib.Scope.Controls.TraceView();
+            this.markerView1 = new FRMLib.Scope.Controls.MarkerView();
+            this.SuspendLayout();
+            // 
+            // scopeView1
+            // 
+            this.scopeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scopeView1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scopeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scopeView1.DataSource = null;
+            this.scopeView1.Location = new System.Drawing.Point(12, 12);
+            this.scopeView1.Name = "scopeView1";
+            scopeViewSettings2.BackgroundColor = System.Drawing.Color.Black;
+            scopeViewSettings2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            scopeViewSettings2.HorizontalDivisions = 10;
+            scopeViewSettings2.HorOffset = 0D;
+            scopeViewSettings2.HorScale = 10D;
+            scopeViewSettings2.VerticalDivisions = 8;
+            this.scopeView1.Settings = scopeViewSettings2;
+            this.scopeView1.Size = new System.Drawing.Size(629, 334);
+            this.scopeView1.TabIndex = 0;
+            // 
+            // traceView1
+            // 
+            this.traceView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.traceView1.DataSource = null;
+            this.traceView1.Location = new System.Drawing.Point(647, 12);
+            this.traceView1.Name = "traceView1";
+            this.traceView1.Size = new System.Drawing.Size(173, 334);
+            this.traceView1.TabIndex = 1;
+            // 
+            // markerView1
+            // 
+            this.markerView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.markerView1.DataSource = null;
+            this.markerView1.Location = new System.Drawing.Point(12, 352);
+            this.markerView1.Name = "markerView1";
+            this.markerView1.Size = new System.Drawing.Size(808, 81);
+            this.markerView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(832, 445);
+            this.Controls.Add(this.markerView1);
+            this.Controls.Add(this.traceView1);
+            this.Controls.Add(this.scopeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private FRMLib.Scope.Controls.ScopeView scopeView1;
+        private FRMLib.Scope.Controls.TraceView traceView1;
+        private FRMLib.Scope.Controls.MarkerView markerView1;
     }
 }
 
--- CSharp/WifiScaleViewer/WifiScaleViewer/Form1.cs
+++ CSharp/WifiScaleViewer/WifiScaleViewer/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using FRMLib.Scope;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,96 @@ namespace WifiScaleViewer
 {
     public partial class Form1 : Form
     {
+        ScopeController scopeController = new ScopeController();
+        Trace weight = new Trace();
+        Trace upper = new Trace();
+        Trace lower = new Trace();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scopeView1.DataSource = scopeController;
+            traceView1.DataSource = scopeController;
+            markerView1.DataSource = scopeController;
+
+
+            weight.Scale = 2;
+            weight.Offset = -85;
+            weight.DrawOption = Trace.DrawOptions.ShowCrosses;
+            weight.Pen = Pens.Yellow;
+
+            upper.Scale = 2;
+            upper.Offset = -85;
+            upper.Pen = Pens.Red;
+
+            lower.Scale = 2;
+            lower.Offset = -85;
+            lower.Pen = Pens.Blue;
+
+            double weightPerDay = 0.5 / 7;
+            double startWeight = 91;
+            double endWeight = 80;
+            double tolerance = 1;
+            int m = (int)((startWeight - endWeight) / weightPerDay);
+
+            DateTime start = new DateTime(2020, 08, 03);
+
+            for(int i = 0; i<m; i++)
+            {
+                DateTime next = start.AddDays(i);
+                upper.Points.Add(next.Ticks, startWeight + tolerance/2 - (weightPerDay * i));
+                lower.Points.Add(next.Ticks, startWeight - tolerance / 2 - (weightPerDay * i));
+            }
+
+            scopeController.Traces.Add(weight);
+            scopeController.Traces.Add(upper);
+            scopeController.Traces.Add(lower);
+
+            ParseFromLogfile(@"\\basnas\docker\connection-server\Log.txt");
+        }
+
+
+
+        void ParseFromLogfile(string file)
+        {
+            double x1 = 8533928;
+            double x2 = 8551186;
+            double y1 = 0;
+            double y2 = 2926.48;
+            double a = (y2 - y1) / (x2 - x1);
+            double b = -a * x1 + y1;
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+
+                while(!reader.EndOfStream)
+                {
+                    string line = Regex.Unescape(reader.ReadLine());
+                    Int64 rawTimestamp;
+                    UInt32 rawMeasurement;
+
+                    if(Int64.TryParse(line.Split(',').First(), out rawTimestamp))
+                    {
+                        DateTime timestamp = new DateTime(rawTimestamp*10000);
+
+                        Match m = Regex.Match(line, @"ScaleReading.+?(\d+)");
+                        if(m.Success)
+                        {
+                            rawMeasurement = UInt32.Parse(m.Groups[1].Value);
+
+                            double val = a * rawMeasurement + b;
+
+                            weight.Points.Add( timestamp.Ticks, val / 1000 );
+                        }
+                    }
+                }
+            }
+
+            scopeView1.FitHorizontalInXDivs(scopeView1.Settings.HorizontalDivisions);
         }
     }
 }
